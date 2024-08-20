6e79f1a31a370ca2010415547f203e67379054c4
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2024-08-21 01:15:33 +0330

    final crud category of admin route

--- app/router/admin/category.ts
+++ app/router/admin/category.ts
@@ -9,7 +9,7 @@ const router = express.Router();
  * @swagger
  *  /admin/category/add:
  *      post:
- *          tags: [Admin-panel]
+ *          tags: [Admin-Panel]
  *          summary: create new category title
  *          parameters:
  *              - in: formData
@@ -30,11 +30,36 @@ const router = express.Router();
 router.post("/add", new CategoryController().createCategory)
 
 
+
+
+/**
+ * @swagger
+ *  /admin/category/update:
+ *      put:
+ *          tags: [Admin-Panel]
+ *          summary: update categorys
+ *          parameters:
+ *              - in: formData
+ *                name: title
+ *                type: string
+ *                required: true
+ *              - in: formData
+ *                name: id
+ *                type: string
+ *                required: true
+ *          responses:
+ *              200:
+ *                  description: success
+ */
+
+router.put("/update", new CategoryController().updateCategory)
+
+
 /**
  * @swagger
  *  /admin/category/parents:
  *      get:
- *          tags: [Amin-Panel]
+ *          tags: [Admin-Panel]
  *          summagry: get All parents of Category or Category Heads
  *          responses:
  *              200:
@@ -50,7 +75,7 @@ router.get("/parents", new CategoryController().getAllParents)
  * @swagger
  *  /admin/category/all:
  *      get:
- *          tags: [Amin-Panel]
+ *          tags: [Admin-Panel]
  *          summagry: get All Categories
  *          responses:
  *              200:
@@ -61,9 +86,27 @@ router.get("/all", new CategoryController().getAllCategories)
 
 /**
  * @swagger
+ *  /admin/category/{id}:
+ *      get:
+ *          tags: [Admin-Panel]
+ *          summagry: get category by id
+ *          parameters:
+ *              -   in: path
+ *                  name: id
+ *                  type: number
+ *                  required: true
+ *          responses:
+ *              200:
+ *                  description: success
+ */
+
+router.get("/:id", new CategoryController().getCategoryById)
+
+/**
+ * @swagger
  *  /admin/category/remove/{id}:
  *      delete:
- *          tags: [Amin-Panel]
+ *          tags: [Admin-Panel]
  *          summagry: remove category with id
  *          parameters:
  *              -   in: path
@@ -81,7 +124,7 @@ router.delete("/remove/:id", new CategoryController().removeCategory)
  * @swagger
  *  /admin/category/children/{parent}:
  *      get:
- *          tags: [Amin-Panel]
+ *          tags: [Admin-Panel]
  *          summagry: get All parents of Category or Category Heads
  *          parameters: 
  *              -   in: path
